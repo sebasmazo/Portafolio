f7d3351994418090d50b6048b568e73f2422327f
sebasmazo <sebastianmazovelez@gmail.com>
2022-04-11 16:49:43 -0500

    Routing ver 1

---
+++ js/routing.js
@@ -0,0 +1,17 @@
+function moveto(id) {
+    switch (id) {
+        case 1:
+            window.scrollTo(0,800)
+            break;
+        case 2:
+            window.scrollTo(0,1100)
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            window.scrollTo(0, 600)
+            break;
+    }
+}
